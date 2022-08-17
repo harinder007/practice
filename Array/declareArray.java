17a7894d1fb81a6683ab1a4ce232b9851375b5a5
harinder007 <harinderyadav1008@gmai.com>
2022-08-17 23:56:32 +0530

    Co-authored-by: Amit Kharod <Amit-kharod@users.noreply.github.com>

---
+++ Array/declareArray.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class declareArray {
+    public static void main(String args[]){
+        int age[]=new int[4];
+
+        //initialize array
+        age[0]=23;
+        age[1]=45;
+        age[2]=67;
+        age[3]=84;
+        System.out.println(age[3]);
+    }
+    
+}
