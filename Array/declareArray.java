b1ed311c8d6826c61eb313413dccf733f815d5c5
harinder007 <harinderyadav1008@gmai.com>
2022-08-18 00:00:51 +0530

    first array program

--- Array/declareArray.java
+++ Array/declareArray.java
@@ -4,7 +4,7 @@ public class declareArray {
     public static void main(String args[]){
         int age[]=new int[4];
 
-        //initialize array
+        //initialize array//
         age[0]=23;
         age[1]=45;
         age[2]=67;
