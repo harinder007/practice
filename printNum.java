6d7b63d665ec49903b5bbfcec8e1eaa9f5880e6d
harinder007 <harinderyadav1008@gmai.com>
2022-09-03 23:07:52 +0530

    no. 1 to 10

---
+++ printNum.java
@@ -0,0 +1,10 @@
+//print no. 1 to 10
+public class printNum {
+    public static void main(String args[]){
+        for(int i=1; i<=10; i++ ){
+            System.out.println(i);
+
+        }
+    }
+    
+}
